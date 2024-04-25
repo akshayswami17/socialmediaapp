1fc24bd08939f51a95dfd1ba4c81754a948c4da2
akshayswami17 <akshayswami1731@gmail.com>
2024-04-25 12:30:24 +0530

    Create social media app.
    1.Added some components from bootstrap.
    2.Make some changes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Sidebar from './components/Sidebar.jsx';
+import FormComponent from './components/FormComponent.jsx';
+import Post from './components/Post.jsx';
+import PostList from './components/PostList.jsx';
+import { useState } from 'react';
 function App() {
+  const [selectedTab , setselectedTab] = useState("Home");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    <div className='App-container'>
+    <Sidebar/>
+    <div className='Content'>
+    <Header /> 
+    { selectedTab === "Home" ? (<PostList/>) : (<FormComponent/>)
+
+    } 
+    
+    
+    <Footer/>
+
     </div>
+
+    
+    </div>
+
+   </div>
   );
 }
 
